84bd84cce620e67bd08644b357ec0eaed93098b6
Christian Clausen <maxclausen@gmail.com>
2020-04-06 16:45:22 +0200

    Extract handleInput

--- index.ts
+++ index.ts
@@ -88,15 +88,19 @@ function update() {
 function handleInputs() {
   while (inputs.length > 0) {
     let current = inputs.pop();
-    if (current === Input.LEFT)
-      moveHorizontal(-1);
-    else if (current === Input.RIGHT)
-      moveHorizontal(1);
-    else if (current === Input.UP)
-      moveVertical(-1);
-    else if (current === Input.DOWN)
-      moveVertical(1);
+    handleInput(current);
   }
+}
+
+function handleInput(input: Input) {
+  if (input === Input.RIGHT)
+    moveHorizontal(1);
+  else if (input === Input.LEFT)
+    moveHorizontal(-1);
+  else if (input === Input.DOWN)
+    moveVertical(1);
+  else if (input === Input.UP)
+    moveVertical(-1);
 }
 
 function updateMap() {
