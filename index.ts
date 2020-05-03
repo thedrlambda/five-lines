f539f108cba4f5eddbecf6a1639a2a2634f02fdd
Christian Clausen <maxclausen@gmail.com>
2020-05-03 17:23:44 +0200

    Remove condition

--- index.ts
+++ index.ts
@@ -211,7 +211,7 @@ class FallStrategy {
       this.falling = new Falling();
       map[y + 1][x] = tile;
       map[y][x] = new Air();
-    } else if (this.falling.isFalling()) {
+    } else {
       this.falling = new Resting();
     }
   }
