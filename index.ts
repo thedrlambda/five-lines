71ff123c4622bdff02aa6640134b0ac42f689510
Christian Clausen <maxclausen@gmail.com>
2020-05-03 14:17:13 +0200

    Inline moveHorizontal

--- index.ts
+++ index.ts
@@ -361,7 +361,7 @@ class Right implements Input {
   isUp() { return false; }
   isDown() { return false; }
   handle() {
-    moveHorizontal(1);
+    map[playery][playerx + 1].moveHorizontal(1);
   }
 }
 
@@ -371,7 +371,7 @@ class Left implements Input {
   isUp() { return false; }
   isDown() { return false; }
   handle() {
-    moveHorizontal(-1);
+    map[playery][playerx - 1].moveHorizontal(-1);
   }
 }
 
@@ -462,10 +462,6 @@ function moveToTile(newx: number, newy: number) {
   map[newy][newx] = new Player();
   playerx = newx;
   playery = newy;
-}
-
-function moveHorizontal(dx: number) {
-  map[playery][playerx + dx].moveHorizontal(dx);
 }
 
 function moveVertical(dy: number) {
