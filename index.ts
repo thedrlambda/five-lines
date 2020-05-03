ee9f010a0e82ee86e9ebaa9e03b99c7bb3e8366a
Christian Clausen <maxclausen@gmail.com>
2020-05-03 16:54:08 +0200

    Simplify condition

--- index.ts
+++ index.ts
@@ -378,9 +378,8 @@ function updateMap() {
 
 function updateTile(x: number, y: number) {
   if ((map[y][x].isStony()
-    && map[y + 1][x].isAir())
-    || (map[y][x].isBoxy()
-      && map[y + 1][x].isAir())) {
+    || map[y][x].isBoxy())
+    && map[y + 1][x].isAir()) {
     map[y][x].drop();
     map[y + 1][x] = map[y][x];
     map[y][x] = new Air();
