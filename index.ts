d4a6f270fac0d13f600ddaa1ea2a6a1c4abad629
Christian Clausen <maxclausen@gmail.com>
2020-05-03 16:53:12 +0200

    Combine ifs

--- index.ts
+++ index.ts
@@ -377,13 +377,10 @@ function updateMap() {
 }
 
 function updateTile(x: number, y: number) {
-  if (map[y][x].isStony()
-    && map[y + 1][x].isAir()) {
-    map[y][x].drop();
-    map[y + 1][x] = map[y][x];
-    map[y][x] = new Air();
-  } else if (map[y][x].isBoxy()
-    && map[y + 1][x].isAir()) {
+  if ((map[y][x].isStony()
+    && map[y + 1][x].isAir())
+    || (map[y][x].isBoxy()
+      && map[y + 1][x].isAir())) {
     map[y][x].drop();
     map[y + 1][x] = map[y][x];
     map[y][x] = new Air();
