9cddc14a8f7e809d633f98cc2b75b887e4862b04
Christian Clausen <maxclausen@gmail.com>
2020-05-03 16:52:08 +0200

    Use drop

--- index.ts
+++ index.ts
@@ -379,11 +379,13 @@ function updateMap() {
 function updateTile(x: number, y: number) {
   if (map[y][x].isStony()
     && map[y + 1][x].isAir()) {
-    map[y + 1][x] = new Stone(new Falling());
+    map[y][x].drop();
+    map[y + 1][x] = map[y][x];
     map[y][x] = new Air();
   } else if (map[y][x].isBoxy()
     && map[y + 1][x].isAir()) {
-    map[y + 1][x] = new Box(new Falling());
+    map[y][x].drop();
+    map[y + 1][x] = map[y][x];
     map[y][x] = new Air();
   } else if (map[y][x].isFalling()) {
     map[y][x].rest();
