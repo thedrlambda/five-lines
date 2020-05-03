61e09845adbeb08ca32244eb1e0148fd2cdbd767
Christian Clausen <maxclausen@gmail.com>
2020-05-03 17:25:30 +0200

    Extract drop

--- index.ts
+++ index.ts
@@ -197,6 +197,9 @@ class FallStrategy {
   isFalling() { return this.falling; }
   update(tile: Tile, x: number, y: number) {
     this.falling = map[y + 1][x].isAir();
+    this.drop(tile, x, y);
+  }
+  private drop(tile: Tile, x: number, y: number) {
     if (this.falling) {
       map[y + 1][x] = tile;
       map[y][x] = new Air();
