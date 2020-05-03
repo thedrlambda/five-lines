66aacbc767764893af0a99579d6e3c27d11eea17
Christian Clausen <maxclausen@gmail.com>
2020-05-03 16:47:24 +0200

    Combine ifs

--- index.ts
+++ index.ts
@@ -374,9 +374,8 @@ function updateTile(x: number, y: number) {
     && map[y + 1][x].isAir()) {
     map[y + 1][x] = new Box(new Falling());
     map[y][x] = new Air();
-  } else if (map[y][x].isFallingStone()) {
-    map[y][x].rest();
-  } else if (map[y][x].isFallingBox()) {
+  } else if (map[y][x].isFallingStone()
+    || map[y][x].isFallingBox()) {
     map[y][x].rest();
   }
 }
