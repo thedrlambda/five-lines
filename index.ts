0b229fe143f6ed48a683a69fdac27705ffe976b6
Christian Clausen <maxclausen@gmail.com>
2020-06-06 17:54:30 +0200

    Introduce private constructor class

--- index.ts
+++ index.ts
@@ -3,6 +3,35 @@ const TILE_SIZE = 30;
 const FPS = 30;
 const SLEEP = 1000 / FPS;
 
+interface RawTileValue { }
+class AirValue implements RawTileValue { }
+class FluxValue implements RawTileValue { }
+class UnbreakableValue implements RawTileValue { }
+class PlayerValue implements RawTileValue { }
+class StoneValue implements RawTileValue { }
+class FallingStoneValue implements RawTileValue { }
+class BoxValue implements RawTileValue { }
+class FallingBoxValue implements RawTileValue { }
+class Key1Value implements RawTileValue { }
+class Lock1Value implements RawTileValue { }
+class Key2Value implements RawTileValue { }
+class Lock2Value implements RawTileValue { }
+class RawTile2 {
+  static readonly AIR = new RawTile2(new AirValue());
+  static readonly FLUX = new RawTile2(new FluxValue());
+  static readonly UNBREAKABLE = new RawTile2(new UnbreakableValue());
+  static readonly PLAYER = new RawTile2(new PlayerValue());
+  static readonly STONE = new RawTile2(new StoneValue());
+  static readonly FALLING_STONE = new RawTile2(new FallingStoneValue());
+  static readonly BOX = new RawTile2(new BoxValue());
+  static readonly FALLING_BOX = new RawTile2(new FallingBoxValue());
+  static readonly KEY1 = new RawTile2(new Key1Value());
+  static readonly LOCK1 = new RawTile2(new Lock1Value());
+  static readonly KEY2 = new RawTile2(new Key2Value());
+  static readonly LOCK2 = new RawTile2(new Lock2Value());
+  private constructor(private value: RawTileValue) { }
+}
+
 enum RawTile {
   AIR,
   FLUX,
